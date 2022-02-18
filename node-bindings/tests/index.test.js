9187ffb4587cf35a65ca9c3c3529f1652bda722d
Matthew Little <zone117x@gmail.com>
2022-02-18 20:36:56 +0100

    feat: unit tests for stacks-devnet-js

--- node-bindings/tests/index.test.js
+++ node-bindings/tests/index.test.js
@@ -4,9 +4,8 @@ const {
     AnchorMode,
   } = require('@stacks/transactions');
 const { StacksTestnet } = require('@stacks/network');
-const { StacksDevnetOrchestrator } = require("../node-bindings/dist/index.js");
-const BigNum = require('bn.js');
-  
+const { StacksDevnetOrchestrator } = require("../dist/index.js");
+
 const orchestrator = new StacksDevnetOrchestrator({
   path: "./clarinet-proj/Clarinet.toml",
   logs: true,
@@ -16,40 +15,40 @@ const orchestrator = new StacksDevnetOrchestrator({
 });
 
 beforeAll(() => {
-  console.log('_____BEFORE ALL___')
+  console.log(new Date().toLocaleTimeString(), '_____BEFORE ALL___');
   return orchestrator.start()
 })
 afterAll(() => orchestrator.stop())
 
 test('Block height changes when blocks are mined', async () => {
-  console.log('___TEST BEGIN___');
+  console.log(new Date().toLocaleTimeString(), '___TEST BEGIN___');
   const network = new StacksTestnet({ url: orchestrator.getStacksNodeUrl() });
 
   // Let's wait for our Genesis block
   var block = orchestrator.waitForStacksBlock();
-  console.log('___GOT STACKS BLOCK___');
+  console.log(new Date().toLocaleTimeString(), '___GOT STACKS BLOCK___');
   // Build a transaction
   const txOptions = {
     recipient: 'ST1SJ3DTE5DN7X54YDH5D64R3BCB6A2AG2ZQ8YPD5',
-    amount: new BigNum(12345),
+    amount: 12345,
     senderKey: '753b7cc01a1a2e86221266a154af739463fce51219d97e4f856cd7200c3bd2a601',
     network,
     memo: 'test memo',
-    nonce: new BigNum(0), // set a nonce manually if you don't want builder to fetch from a Stacks node
-    fee: new BigNum(200), // set a tx fee if you don't want the builder to estimate
+    nonce: 0, // set a nonce manually if you don't want builder to fetch from a Stacks node
+    fee: 200, // set a tx fee if you don't want the builder to estimate
     anchorMode: AnchorMode.OnChainOnly
   };
-  console.log('___MAKE STX TX___');
+  console.log(new Date().toLocaleTimeString(), '___MAKE STX TX___');
   const transaction = await makeSTXTokenTransfer(txOptions);
 
-  console.log('___BROADCAST STX TX___');
+  console.log(new Date().toLocaleTimeString(), '___BROADCAST STX TX___');
   // Broadcast transaction to our Devnet stacks node
   await broadcastTransaction(transaction, network);
   
-  console.log('___WAIT FOR NEXT STACKS BLOCK___');
+  console.log(new Date().toLocaleTimeString(), '___WAIT FOR NEXT STACKS BLOCK___');
   // Wait for the next block
   block = orchestrator.waitForStacksBlock();
 
   // Ensure that the transaction was included in the block
-  console.log(`Next Block: ${JSON.stringify(block)}`);
+  console.log(new Date().toLocaleTimeString(), `Next Block: ${JSON.stringify(block)}`);
 }, 12_500_000)
